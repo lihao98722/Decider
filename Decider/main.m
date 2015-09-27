0d0c67f0d6a0523a7a2fca9f6ea300bc1309db63
lihao98722 <lihao98722@gmail.com>
2015-09-27 00:15:33 -0400

    Support Multi-language

---
+++ Decider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Decider
+//
+//  Created by Howie Li on 6/7/14.
+//  Copyright (c) 2014 me.howieli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DeciderAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DeciderAppDelegate class]));
+    }
+}
